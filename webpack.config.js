2ec2f6a4d494b3a4ac2f9f5a225725e96b1a65cb
Jarod Peachey <jwpeachey107@aol.com>
2019-04-04 17:39:48 -0400

    Move index.html to /public folder Override jsonserver page

--- webpack.config.js
+++ webpack.config.js
@@ -34,7 +34,7 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       filename: 'index.html',
-      template: path.join(__dirname, 'src', 'index.html'),
+      template: path.join(__dirname, 'public', 'index.html'),
     }),
   ],
   resolve: {
