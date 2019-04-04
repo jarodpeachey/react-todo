c89194b4dca7c4095bb00327fcc991d078940873
Jarod Peachey <jwpeachey107@aol.com>
2019-04-04 01:05:03 -0400

    Add about page and header navigation components

---
+++ src/components/layout/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function About() {
+   return (
+      <section className="bg-light">
+         <div className="container py-4">
+            <h2 className="mb-2">About ToDo</h2>
+            <p className="mb-0">This is a todo list app from a React Crash Course</p>
+         </div>      
+      </section>
+   );
+}
+
+export default About;
---
+++ src/components/layout/Header.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Header () {
+   return (
+      <div className="navbar bg-primary">
+         <div className="navbar-content">
+            <div className="navbar-left">
+               <h1 className="m-0">Todo List</h1>
+            </div>
+            <div className="navbar-right">
+               <ul className="menu">
+                  <li className="menu-item">
+                     <Link to="/">Home</Link>
+                  </li>
+                  <li className="menu-item">
+                     <Link to="/about">About</Link>
+                  </li>
+               </ul>
+            </div>
+         </div>
+      </div>
+   )
+}
+
+export default Header;
