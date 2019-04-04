dc865b1c98a10d1fd712a6ecdfa38ffe908fef90
Jarod Peachey <jwpeachey107@aol.com>
2019-04-04 16:45:33 -0400

    Update

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,10 @@
 module.exports = {
-   presets: [
-       '@babel/preset-env',
-       '@babel/preset-react'
-   ],
-   plugins: [
-       '@babel/plugin-proposal-class-properties'
-   ]
+  presets: [
+    '@babel/preset-env',
+    '@babel/preset-react',
+  ],
+  plugins: [
+    '@babel/plugin-proposal-class-properties',
+    'transform-class-properties',
+  ],
 };
